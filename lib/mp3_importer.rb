f1062e62319f1db3ca0af16e293db389ffa48af9
emi kojima <emi8emi@gmail.com>
2018-03-02 04:24:12 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,14 @@
+require 'pry'
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(filepath)
+    @path = filepath
+  end
+
+  def files
+    @files = Dir.entries(@path)
+    @files.delete_if {|file| file == "." || files == ".."}
+  end
+
+end
